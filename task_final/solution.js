393fabeeb1d74b5a5dd8797e848c01f6800a97fb
Andrei Miagkov <mats07@mail.ru>
2023-10-01 19:15:53 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,6 +33,8 @@
 //    }
 // }
 
+
+/*
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
 
@@ -48,6 +50,25 @@ function sendRequest(name, phone, address, goods, sum) {
     data.client = 'Иван';
 
     let jsonData = JSON.stringify(data);
+
+    return jsonData;
+}
+*/
+
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {goods: [], order: {}};
+
+    let countOfGoods = goods.length;
+
+    for (let i = 0; i < countOfGoods; i+=1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
+    }
+
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
+    data.order.sum = sum;
+    data.client = name + ' ' + phone;
+
+    let jsonData = JSON.stringify({data:data});
 
     return jsonData;
 }
